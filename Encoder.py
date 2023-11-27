b984fb368b99da4c38457e40aff971671d121181
poberto <pob7@pitt.edu>
2023-11-26 23:13:21 -0500

    added more comments for convolutional encoder class

--- Encoder.py
+++ Encoder.py
@@ -3,17 +3,22 @@ from helpers import InitOutputs
 #convolutional encoder class
 class ConvolutionalEncoder:
     def __init__(self, constraint, polynomials):
-        self.constraint = constraint
+        #constraint length of the encoder, the length of the sliding window and number of message bits used for the XOR operations in encoding process
+        #Polynomials are generator polynomials that define how the XOR operations on the message bits are performed
+        self.constraint = constraint      
         self.polynomials = [int(p, 2) for p in polynomials]
-        self.outputs = []
-        InitOutputs(self)
+        self.outputs = [] #each state in the encoder's state machine has a unique output, this holds those outputs flor each state
+        InitOutputs(self) # This function basically draws the state machine of the encoder so that each output of each possible state is defined when the encoder is initialized
+        #each state represents a particular "memory" of previous input bits 
 
     #calculates next state based on current state and input
+    # this function examines the current state and follows the defined state machine of the encoder to the next state 
     def NextState(self, CurrentState, input):
         return (CurrentState >> 1) | (input << (self.constraint - 2))
 
     #encodes the input bits
     def encode(self, bits):
+        #starts here as 0 because there are no previous input bits and initializes a string to hold output bits
         state = 0
         encoded = ""
         for bit in bits:
