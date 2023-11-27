0543665ea967295e639ff0e0d7cc9f273463997b
poberto <pob7@pitt.edu>
2023-11-27 00:16:35 -0500

    added more comments to helper functions

--- helpers.py
+++ helpers.py
@@ -3,6 +3,8 @@ import random
 import matplotlib.pyplot as plt
 
 # calculates Hamming Distance
+#Hamming diatance is the number of bits in the number that differ freom the expected
+#Since this is Hard decision decoding, the branch metric used is the Hamming distance 
 def HammingDistance(x, y):
     assert len(x) == len(y)
     distance = 0
@@ -12,21 +14,25 @@ def HammingDistance(x, y):
     return distance
 
 #calculates output bits for each state
+#This function basically draws the state machine of the encoder so that each output of each possible state is defined when the encoder is initialized
 def InitOutputs(self):
     self.outputs = [0] * (1 << self.constraint)
     for i in range(len(self.outputs)):
         for j, polynomial in enumerate(self.polynomials):
+            #This reverses the bits of the polynomial so it correctly matches the state machine
             reversed_polynomial = ReverseBits(self.constraint, polynomial)
             input = i
             output = 0
+            #This performs the XOR operations accoreding to the encoer's state mechine, defined by the constraint and polynomials
             for k in range(self.constraint):
                 output ^= (input & 1) & (reversed_polynomial & 1)
                 reversed_polynomial >>= 1
                 input >>= 1
+            #updates the outputs for this state
             self.outputs[i] += output << j
 
 
-#reverses bit order
+#reverses bit order. This is needed when tracing backwards through the trellis to find the MLP
 def ReverseBits(bitnum, input):
     output = 0
     while bitnum > 0:
@@ -35,17 +41,18 @@ def ReverseBits(bitnum, input):
         bitnum -= 1
     return output
 
-# counts number of errors corrected 
+# counts number of errors corrected errors by decoder
+#finds them by comparing input, recieved, and decoded bits
 def CountCorrected(InputBits, RecievedBits, DecodedBits):
     num_corrected_errors = sum(i != r and i == d for i, r, d in zip(InputBits, RecievedBits, DecodedBits))
     return num_corrected_errors
 
-#calculates bit errro rate
+#calculates bit errro rate, the proportion of bits incorrectly decoded
 def CalculateBER(InputBits, DecodedBits):
     errors = sum(i != d for i, d in zip(InputBits, DecodedBits))
     return errors / len(InputBits)
 
-#generates random bit string
+#generates random bit string, just used for randomizing input when testing
 def RandomBits(length):
     return [random.randint(0, 1) for _ in range(length)]
 
@@ -55,6 +62,7 @@ def PlotTrellis(trellis_data, states, bitnum, MLP=None):
 
     for bit in range(bitnum):
         for state in range(states):
+            #this plots the state transitions at each time step
             NextState = trellis_data[bit * states + state]
             NextBit, NextState = NextState  # Extract individual values
             plt.plot([bit, bit + 1], [state, NextState], marker='s', markersize=11, linestyle='--', linewidth=0.6, color='k')
